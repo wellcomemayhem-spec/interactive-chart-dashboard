f5acb2d5431ad32c9711393a19b6fa5bd29815b0
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-09-24 22:05:43 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Interactive Chart Dashboard</h1>
+        {/* Chart components will be added here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
